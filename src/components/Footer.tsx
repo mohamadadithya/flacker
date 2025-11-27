14f019468f0cbf2eed21681b243d70c8cb803a8d
Mohamad Adithya <mohamadadithya@gmail.com>
2025-11-27 13:30:59 +0700

    feat: add Footer component with author attribution

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="absolute bottom-5 left-2/4 -translate-x-2/4">
+      <p className="text-center text-sm text-base-content text-balance">
+        Made with ❤️ by{" "}
+        <a
+          href="https://www.haloadit.com"
+          className="link link-hover text-primary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mohamad Adithya
+        </a>
+      </p>
+    </footer>
+  );
+}
