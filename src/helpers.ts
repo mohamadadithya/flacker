a0c3135b48d031c9206b102d1ff0f587dea0cc1b
Mohamad Adithya <mohamadadithya@gmail.com>
2025-11-28 23:56:52 +0700

    refactor(app): update download blob helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -15,30 +15,16 @@ function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-const isAndroid = /Android/i.test(navigator.userAgent);
-
 function downloadBlob(blob: Blob, filename: string) {
-  if (!blob || blob.size === 0) {
-    console.error("blob is empty or invalid", blob);
-    throw new Error("FLAC File is not valid!");
-  }
-
   const url = URL.createObjectURL(blob);
-
-  if (isAndroid) {
-    window.open(url, "_blank");
-
-    setTimeout(() => {
-      URL.revokeObjectURL(url);
-    }, 30_000);
-    return;
-  }
-
   const a = document.createElement("a");
 
   a.href = url;
-  a.download = filename || "output.flac";
+  a.download = filename;
+  a.target = "_blank";
+  a.rel = "noopener noreferrer";
   a.style.display = "none";
+
   document.body.appendChild(a);
 
   a.click();
@@ -46,8 +32,9 @@ function downloadBlob(blob: Blob, filename: string) {
   const timeout = setTimeout(() => {
     URL.revokeObjectURL(url);
     a.remove();
-  }, 30_000);
+  }, 30_000); // 30s
 
+  // Return cleanup function (optional to call)
   return () => {
     clearTimeout(timeout);
     URL.revokeObjectURL(url);
