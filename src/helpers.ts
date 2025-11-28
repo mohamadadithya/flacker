d00e75eb7bbef1eb2d0d13a05df6c091c5cc0eb8
Mohamad Adithya <mohamadadithya@gmail.com>
2025-11-28 23:17:22 +0700

    refactor(app): add revoke delay on download blob helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -15,13 +15,28 @@ function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-function downloadBlob(blob: Blob, name: string) {
+function downloadBlob(blob: Blob, filename: string) {
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
+
   a.href = url;
-  a.download = name;
+  a.download = filename;
+  a.style.display = "none";
+  document.body.appendChild(a);
+
   a.click();
-  URL.revokeObjectURL(url);
+
+  const timeout = setTimeout(() => {
+    URL.revokeObjectURL(url);
+    a.remove();
+  }, 30_000); // 30s
+
+  // Return cleanup function (optional to call)
+  return () => {
+    clearTimeout(timeout);
+    URL.revokeObjectURL(url);
+    a.remove();
+  };
 }
 
 export { fileSetter, cn, downloadBlob };
